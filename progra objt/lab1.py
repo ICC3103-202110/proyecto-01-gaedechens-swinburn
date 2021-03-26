bca24a4064893445bfa8e10d139082da153b05ee
cgaedechens <csgaedechens@miuandes.cl>
2021-03-25 22:55:31 -0300

    programa totalmente funcional, falta arreglar los input

--- progra objt/lab1.py
+++ progra objt/lab1.py
@@ -16,7 +16,7 @@ pointsp2= 0 #puntaje player 2
 while pointsp1 + pointsp2 < pairs:
 
     #####PRIMERA JUGADA#####
-    while True :
+    while len(cardsp1) > 0 :
         print ("Player 1 turn, here are the cards:")
         censoredcardsp1 = []
         for i in cardsp1:
@@ -68,13 +68,76 @@ while pointsp1 + pointsp2 < pairs:
         if chosencard1 == chosencard2:
             print("You have won a point, its your turn again")
             pointsp1+=1
-            cardsp1.pop(chosingcards1) #eliminando las cartas elegidas si esque son iguales
-            cardsp1.pop(chosingcards2)
+            cardsp1.remove(chosencard1) #eliminando las cartas elegidas si esque son iguales
+            cardsp1.remove(chosencard2)
+            key = False
+            
+        else:
+            print("You lose this turn")
+            key = True
+            break
+
+    while len(cardsp1) > 0 and key == True :
+        print ("Player 2 turn, here are the cards:")
+        censoredcardsp1 = []
+        for i in cardsp1:
+            censoredcardsp1.append("*")
+        print (censoredcardsp1)
+        print("With the number 0 the first card of the list is selected, with 1 the second and so on, select the card to turn: ")
+        chosingcards1= int(input()) #elige la posicion de la carta a dar vuelta
+        chosencard1 = cardsp1[chosingcards1] #esta es la carta que se dio vuelta
+        thecard = list(str(cardsp1[chosingcards1])) #convierte la carta que se dio vuelta en una lista para despues sumar listas y censurar las demas
+
+        listh = [] #separa la lista para censurar (hasta esa carta)
+        listd = [] #separa la lista para censurar (desde esa carta en adelante)
+        liste = [] #separa la lista para censurar (entremedio de las cartas)
+        for i in cardsp1[:chosingcards1]:
+            listh.append("*")
+        for i in cardsp1[chosingcards1+1:]:
+            listd.append("*")
+        listrep = listh + thecard + listd  #lista con todo censurado excepto la carta a mostrar
+        print (listrep)
+        print ("Now select another card to flip and see if it is his pair")
+        chosingcards2 = int(input()) #segunda carta a dar vuelta
+
+        chosencard2 = cardsp1[chosingcards2] # esta es la segunda carta que se dio vuelta
+        thecard2 = list(str(cardsp1[chosingcards2])) #convierte la segunda carta que se dio vuelta en una lista para despues sumar listas y censurar las demas
+
+        listh = [] #separa la lista para censurar (hasta esa carta)
+        listd = [] #separa la lista para censurar (desde esa carta en adelante)
+        liste = [] #separa la lista para censurar (entremedio de las cartas)
+        if chosingcards1 < chosingcards2: 
+            for i in cardsp1[:chosingcards1]:
+                listh.append("*")
+            for i in cardsp1[chosingcards1+1:chosingcards2]:
+                liste.append("*")
+            for i in cardsp1[chosingcards2+1:]:
+                listd.append("*")
+            listrep = listh+thecard+liste+thecard2+listd
+            print (listrep)
+
+        if chosingcards1 > chosingcards2:
+            for i in cardsp1[:chosingcards2]:
+                listh.append("*")
+            for i in cardsp1[chosingcards2+1:chosingcards1]:
+                liste.append("*")
+            for i in cardsp1[chosingcards1+1:]:
+                listd.append("*")
+            listrep = listh+thecard2+liste+thecard+listd
+            print (listrep)    
+
+        if chosencard1 == chosencard2:
+            print("You have won a point, its your turn again")
+            pointsp2+=1
+            cardsp1.remove(chosencard1) #eliminando las cartas elegidas si esque son iguales
+            cardsp1.remove(chosencard2)
+            continue
         else:
             print("You lose this turn")
             break
+if pointsp2 < pointsp1:
+    print ("Player 1 Wins!!")
+if pointsp2 > pointsp1:
+    print ("Player 2 Wins!!")
 
-    while True :
-        print ("Player 2 turn, here are the cards:")
-        break
 
