c78a12e54dcdd75b38459b4f99d39dd3801cb880
cgaedechens <csgaedechens@miuandes.cl>
2021-04-19 01:20:42 -0400

    creamos dos heroes para empezar a probar y estamos casi logrando jugar una ronda, pero se nos cae por un typerror

--- POO_Proyect1/classes.py
+++ POO_Proyect1/classes.py
@@ -14,13 +14,50 @@ class Hero(ABC):
     @abstractmethod
     def counter_action(self):#abstract method because each character is going to have a different method
         pass
-"""    
+  
 class Duke(Hero):
     def action(self,Dealer,player):
-        
+        did_player_shown_duke = Dealer.check_for_bluff(player)
+        #did_player_shown_duke = False means no one doubt it
+        #did_player_shown_duke = True means SOMEONE DOUBT IT and he lost one card or doubt it succsseessffuuyllyy one doubt it
+        if did_player_shown_duke == False:
+            if len(Dealer.bag_coins) > 2:
+                for i in range(0,3):
+                    player.pocket_coins.append("coin")
+                for i in range(0,3):
+                    Dealer.bag_coins.pop(0)
+                return True
+        else:
+            #TO DO
+            #replase the hero duke of the player
+            print("!replacing duke hero!")
+            pass
+        return False
+    def counter_action(self,action_name):
+        if action_name == 'Foreign Aid':
+            return True
+        return False
 class Assasin(Hero):
-    def action(self,Dealer,target_player,player):""" #something like this i think
-        
+    def action(self,Dealer,target_player,player): #something like this i think
+        for i in range(0,3):
+            player.pocket_coins.pop(0)    
+        insert_three(Dealer)
+        print("--other player chat---")
+        print("Hello player "+target_player.get_name()+ "\n the player "+ player.get_name() + "  has killed a influence of yours, \n do you think it is a bluff? \n [1] Yes its a bluff \n [2] No its not a bluff")
+        target_player_option = input("Response : ")
+        if target_player_option == "1":
+            if player.hero1.hero_name == 'Assasin' or player.hero2.hero_name == 'Assasin':
+                target_player.hero1 = None
+                target_player.hero2 = None
+            else:
+                player.get_rid_of_a_card()
+        else:
+            target_player.get_rid_of_a_card()      
+        return True
+    def counter_action(self,action_name):
+        if action_name == 'Foreign Aid':
+            return True
+        return False
 class Player:
     def __init__(self,name,hero1,hero2):#builder of players
         self.name = name
@@ -28,25 +65,72 @@ class Player:
         self.hero2 = hero2
         self.pocket_coins = draw_two()
     
+    def lose_influence(self,number):
+        if number == "1":
+            self.hero1 = None
+        elif number == "2":
+            self.hero2 = None
+
     def get_name(self): #get name obviously
         return self.name
+
+    def get_rid_of_a_card(self):
+        print("Hello player "+self.get_name())
+        if self.hero1.hero_name and self.hero2.hero_name:#####PROBLEM HERE 'NoneType' object has no attribute 'hero_name', #####
+            print("Chose what influence to lose \n [1] "+str(self.hero1.hero_name)+" \n [2] "+str(self.hero2.hero_name))
+            lose = input("Enter your influence number : ")
+            self.lose_influence(lose)
+        elif self.hero1.hero_name:
+            print("Chose what influence to lose \n [1] "+str(self.hero1.hero_name))
+            lose = input("Enter your influence number : ")
+            self.hero1 = None
+        elif self.hero2.hero_name:
+            print("Chose what influence to lose \n [2] "+str(self.hero2.hero_name))
+            lose = input("Enter your influence number : ")
+            self.hero2 = None
+
+    def show_options(self):
+        print("Showing current status for player " + self.get_name())
+        if self.hero1:
+            print("Hero 1  ["+self.hero1.hero_name+"]")
+        if self.hero2:
+            print("Hero 2  ["+self.hero2.hero_name+"]") 
+        print("Total coins in the BAAAGGG "+ str(len(self.pocket_coins)))
 
 class Dealer:
     def __init__(self,player_amount): #the dealer is like our bank
         buffer_token = Token("coin")
         self.bag_coins = [buffer_token for i in range(0,50)] #creating all the coins of the game
+        duke = Duke(hero_name='Duke',
+                     description='1 duke')
+        assasin = Assasin(hero_name='Assasin',
+                     description='1 assasin')
+                     #creating some heroes to try the game
         self.players = []#the list of the players
         for i in range(0,player_amount): #creating players
-            player_buffer = Player(str(i+1),"hero1","hero2")
+            player_buffer = Player(str(i+1),duke,assasin)
             self.players.append(player_buffer)
 
     def show_players(self): #this is to show all players
         print("Showing players! ")
-        for i in self.players:
+        for i in self.players:#BEFORE THIS WE NEED TO INCLUDE ONLY THE PLAYERS THAT STILL ALIVE
             print("Player "+i.get_name())
 
     def get_player_count(self): #counter of players
-        return len(self.players)    
+        return len(self.players)  
+
+    def check_for_bluff(self,player):
+        did_player_shown_duke = False
+        for player_buffer in self.players:
+            if player_buffer != player:
+                print("\n ----- \n Hello player "+player_buffer.get_name() + " \n do you think the player "+ player.get_name()+" is bluffing on that duke moove? \n [1] Yes is bluffing \n [2] No he is not bluffing")
+                player_buffer_option = input("Insert option : ")
+                if player_buffer_option == "1":
+                    if player.hero1.hero_name == 'Duke' or player.hero2.hero_name == 'Duke':
+                        did_player_shown_duke = True
+                        player_buffer.get_rid_of_a_card()       
+                    break
+        return did_player_shown_duke
 
 class Token: #the coin of the game
     def __init__(self,name):
@@ -58,3 +142,9 @@ def draw_two():#this is to give the players 2 coins at the start of the game
         buffer_token = Token("coin")
         coins.append(buffer_token)
         return coins
+
+def insert_three(Dealer): #paying the dealer 3 coins
+    for i in range(0,3):
+        buffer_token = Token("coin")
+        Dealer.bag_coins.append(buffer_token)
+
--- POO_Proyect1/main.py
+++ POO_Proyect1/main.py
@@ -1,8 +1,56 @@
 from classes import * 
 
+def showturn(player_phase):
+    print("--------")
+    print("Turn of player "+ str(player_phase+1))
+
+def show_and_get_option(dealer):
+    posibles = [str(i) for i in range(1,8)]
+    while(True):
+        print("Options \n [1] Income \n [2] Foreign Aid \n [3] Coup \n [4] Tax (Duke required) \n [5] Assassinate *3 coins* (assasin required)\n [6] Exchange (Ambassador required) \n [7] Steal (captain required)")
+        opcion = input("Option : ")
+        if opcion in posibles:
+            return opcion
+        else:
+            print("Invalid option...")
+
+
+
 if __name__=='__main__':
     print("Welcome to Coup! ")
     amount_of_players = int(input("Enter player amount :")) 
     dealer = Dealer(amount_of_players) #creating instance for Dealer 
     dealer.show_players()
-    
+    phase_counter = 0
+    while True:
+        #current player
+        player_phase = phase_counter%amount_of_players
+        if dealer.players[player_phase].hero1 != None or dealer.players[player_phase].hero2 != None: 
+            showturn(player_phase)
+            
+            #actual turn
+            print(dealer.players[player_phase].get_name())
+            dealer.players[player_phase].show_options()
+            
+            #Current player ----> dealer.players[player_phase]
+            
+            #option choosing part
+            opcion = show_and_get_option(dealer)
+            if opcion == "5":
+                
+                #assasination menu
+                while True:
+                    print("Posible targets bellow")
+                    dealer.show_players()
+                    target_option = input("Select player ")
+                    if int(target_option) <= (dealer.get_player_count()):
+                        assasin_buffer = Assasin("assasin","1 assasin")
+                        assasin_buffer.action(dealer,dealer.players[int(target_option)-1],dealer.players[player_phase])
+                        break
+                    else:
+                        print("Incorect player election... returning")
+            elif opcion == "4":
+                duke_buffer = Duke("duke","1 duke")
+                duke_buffer.action(dealer,dealer.players[player_phase])
+        phase_counter +=1
+        
